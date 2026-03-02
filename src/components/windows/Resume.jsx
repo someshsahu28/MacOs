6ec91af363540b0cd3f12db2436d4824d6b00a4b
someshsahu28 <someshsahu.sagar@gmail.com>
2026-03-02 22:18:08 +0530

    update the resume and mac window

--- src/components/windows/Resume.jsx
+++ src/components/windows/Resume.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import MacWindow from './MacWindow'
-import "./resume.scss"
+import React from "react";
+import MacWindow from "./MacWindow";
+import "./resume.scss";
 
 const Resume = ({ windowName, setWindowsState }) => {
-    return (
-        <MacWindow windowName={windowName} setWindowsState={setWindowsState} >
-            <div className="resume-window">
-                <iframe
-  src="/resume.pdf"
-  width="100%"
-  height="100%"
-  style={{ border: "none" }}
-  title="Resume"
-/>
-            </div>
-        </MacWindow>
-    )
-}
+  return (
+    <MacWindow
+      windowName={windowName}
+      setWindowsState={setWindowsState}
+      width="60vw"
+      height="70vh"
+    >
+      <div className="resume-window">
+        <iframe
+          src="/resume.pdf"
+          title="Resume"
+        />
+      </div>
+    </MacWindow>
+  );
+};
 
-export default Resume
+export default Resume;
