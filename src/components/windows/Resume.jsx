45ef29e8759ef3aa671e7715c20e26072ecd2dd2
someshsahu28 <someshsahu.sagar@gmail.com>
2026-03-03 17:38:30 +0530

    update the pdf  render issue

--- src/components/windows/Resume.jsx
+++ src/components/windows/Resume.jsx
@@ -6,7 +6,13 @@ const Resume = ({ windowName, setWindowsState }) => {
     return (
         <MacWindow windowName={windowName} setWindowsState={setWindowsState} >
             <div className="resume-window">
-                <iframe src="/resume.pdf" frameborder="0"></iframe>
+                <iframe
+  src="/resume.pdf#view=FitH"
+  width="100%"
+  height="100%"
+  style={{ border: "none" }}
+  title="Resume"
+/>
             </div>
         </MacWindow>
     )
